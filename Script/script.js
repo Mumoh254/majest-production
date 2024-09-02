228e6c945ca2aada4ce3b73b7ba165cc4f694a74
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-09-03 00:16:40 +0300

    u

--- Script/script.js
+++ Script/script.js
@@ -38,7 +38,7 @@
 
 
 // Get the modal
-var modal = document.getElementById("shoeModal");
+var modalShow = document.getElementById("shoeModal");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -46,18 +46,18 @@ var span = document.getElementsByClassName("close")[0];
 // Show the modal after a delay (e.g., 2 seconds after page load)
 window.onload = function() {
     setTimeout(function() {
-        modal.style.display = "block";
+        modalShow.style.display = "block";
     }, 2000);
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modelShow.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modalShow) {
+        modalShow.style.display = "none";
     }
 }
